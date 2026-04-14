c42bb55a46060a0b4f3a021a3a154972005901a0
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-04-14 14:39:17 +0200

    Add project files.

---
+++ Weather/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Weather.Data;//Connects to DataContext
+using Weather.Models; //Connects to User model
+
+namespace Weather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+             _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>> AddUser(User user)
+        {
+           _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+       
+    }
+}
---
+++ Weather/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Weather.Models
+{
+    public class User
+    {
+        [Required] public string Id { get; set; }
+        [Required]public string Username { get; set; } = string.Empty;
+        [Required] public string Email { get; set; } = string.Empty;
+    }
+}
