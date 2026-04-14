d7a40d6d1e413489976e2bfd744bdddec292bf63
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-04-14 15:00:04 +0200

    changed Id column to int instead of string

--- Weather/Migrations/20260414124618_NewCreate.Designer.cs
+++ Weather/Migrations/20260414124618_NewCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Weather.Data;
 
@@ -10,9 +11,11 @@ using Weather.Data;
 namespace Weather.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260414124618_NewCreate")]
+    partial class NewCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Weather/Models/User.cs
+++ Weather/Models/User.cs
@@ -4,7 +4,7 @@ namespace Weather.Models
 {
     public class User
     {
-        [Required] public string Id { get; set; }
+        [Required] public int Id { get; set; }
         [Required]public string Username { get; set; } = string.Empty;
         [Required] public string Email { get; set; } = string.Empty;
     }
